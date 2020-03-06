1fe62a6c7189feae07a126a4b7e07f24afab2614
Woody Lee <woody.lee@ef.com>
2020-03-06 22:13:44 +0800

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ echo "Install start..."
 # install oh-my-zsh
 [ -d ~/.oh-my-zsh ] || git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] || git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-[ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 
 
 echo "✔ oh-my-zsh is installed"
 
